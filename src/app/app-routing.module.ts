edc110e045e9279e4fc8e3cf0f3cba4fcee7cf51
Tamara-moran <tamara.moran@softwareone.com>
2022-02-24 13:45:57 +0100

    Lab07- Navigation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {FaqsComponent} from './faqs/faqs.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'faqs',
+    component: FaqsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
